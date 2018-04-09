4aaf4ff4b91ed5b5426a14f0b36ef196d3533f69
keanedawg <cameronfife@hotmail.com>
2018-04-09 02:46:53 -0600

    code cleanup

--- client.js
+++ client.js
@@ -15,11 +15,9 @@ client.connect(PORT, HOST, function() {
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', function(data) {
-    
     console.log('DATA: ' + data);
     // Close the client socket completely
     client.destroy();
-    
 });
 
 // Add a 'close' event handler for the client socket
